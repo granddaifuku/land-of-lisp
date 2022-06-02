b32eb317e3d35695a8681b3d92a452dadd835d3e
granddaifuku <daifukuformac@gmail.com>
2022-06-02 23:11:54 +0900

    feat: implement vs human version

--- dice-of-doom.lisp
+++ dice-of-doom.lisp
@@ -21,7 +21,7 @@
 						do (princ "  "))
 				  (loop for x below *board-size*
 						for hex = (aref board (+ x (* *board-size* y)))
-						do (format t "~a~a " (player-letter (first hex))
+						do (format t "~a-~a " (player-letter (first hex))
 								   (second hex))))))
 
 (defun game-tree (board player spare-dice first-move)
@@ -71,6 +71,72 @@
 						   (unless (zerop (mod pos *board-size*))
 							 (list (1- up) (1- pos)))
 						   (unless (zerop (mod (1+ pos) *board-size*))
-							 (list (1+ pos) (1 + down))))
+							 (list (1+ pos) (1+ down))))
 		  when (and (>= p 0) (< p *board-hexnum*))
 			collect p)))
+
+(defun board-attack (board player src dst dice)
+  (board-array (loop for pos from 0
+					 for hex across board
+					 collect (cond ((eq pos src) (list player 1))
+								   ((eq pos dst) (list player (1- dice)))
+								   (t hex)))))
+
+(defun add-new-dice (board player spare-dice)
+  (labels ((f (lst n)
+			 (cond ((null lst) nil)
+				   ((zerop n) lst)
+				   (t (let ((cur-player (caar lst))
+							(cur-dice (cadar lst)))
+						(if (and (eq cur-player player) (< cur-dice *max-dice*))
+							(cons (list cur-player (1+ cur-dice))
+								  (f (cdr lst) (1- n)))
+							(cons (car lst) (f (cdr lst) n))))))))
+	(board-array (f (coerce board 'list) spare-dice))))
+
+(defun play-vs-human (tree)
+  (print-info tree)
+  (if (caddr tree)
+	  (play-vs-human (handle-human tree))
+	  (announce-winner (cadr tree))))
+
+(defun print-info (tree)
+  (fresh-line)
+  (format t "current player = ~a" (player-letter (car tree)))
+  (draw-board (cadr tree)))
+
+(defun handle-human (tree)
+  (fresh-line)
+  (princ "choose your move:")
+  (let ((moves (caddr tree)))
+	(loop for move in moves
+		  for n from 1
+		  do (let ((action (car move)))
+			   (fresh-line)
+			   (format t "~a. " n)
+			   (if action
+				   (format t "~a -> ~a" (car action) (cadr action))
+				   (princ "end turn"))))
+	(fresh-line)
+	(cadr (nth (1- (read)) moves))))
+
+(defun winners (board)
+  (let* ((tally (loop for hex across board
+					  collect (car hex)))
+		 (totals (mapcar (lambda (player)
+						   (cons player (count player tally)))
+						 (remove-duplicates tally)))
+		 (best (apply #'max (mapcar #'cdr totals))))
+	(mapcar #'car
+			(remove-if (lambda (x)
+						 (not (eq (cdr x) best)))
+					   totals))))
+
+(defun announce-winner (board)
+  (fresh-line)
+  (let ((w (winners board)))
+	(if (> (length w) 1)
+		(format t "The game is a tie between ~a" (mapcar #'player-letter w))
+		(format t "The winner is ~a" (player-letter (car w))))))
+
+(play-vs-human (game-tree (gen-board) 0 0 t))
